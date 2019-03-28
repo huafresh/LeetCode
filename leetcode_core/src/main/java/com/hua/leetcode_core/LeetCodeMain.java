f892a3a7400575e8733613e34f02107aacf320ad
huafresh <2587168099>
2019-03-28 09:01:48 +0800

    转化成小写字母

--- leetcode_core/src/main/java/com/hua/leetcode_core/LeetCodeMain.java
+++ leetcode_core/src/main/java/com/hua/leetcode_core/LeetCodeMain.java
@@ -8,7 +8,6 @@ public class LeetCodeMain {
 
 
     /**
-     * 宝石与石头
      * <a href = "https://leetcode-cn.com/problems/jewels-and-stones/">宝石与石头</a>
      */
     public int numJewelsInStones(String strJ, String strS) {
@@ -20,5 +19,42 @@ public class LeetCodeMain {
         }
         return count;
     }
+
+    public static final int LOWER_CASE_MIN_VALUE = 0x61;
+    public static final int LOWER_CASE_MAX_VALUE = 0x7A;
+    public static final int UPPER_CASE_MIN_VALUE = 0x41;
+    public static final int UPPER_CASE_MAX_VALUE = 0x5A;
+    public static final int LOWER_UPPER_CASE_INTERVAL_VALUE = 0x20;
+
+    /**
+     * <a href = "https://leetcode-cn.com/problems/to-lower-case/submissions/">转换成小写字母</a>
+     */
+    public String toLowerCase(String str) {
+        final StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (isNeedTransform(c)) {
+                c += LOWER_UPPER_CASE_INTERVAL_VALUE;
+                builder.append(c);
+            } else {
+                builder.append(c);
+            }
+        }
+        return builder.toString();
+    }
+
+    private static boolean isNeedTransform(char c) {
+        return isCharacter(c) && isUpperCase(c);
+    }
+
+    private static boolean isUpperCase(char c) {
+        return c < LOWER_CASE_MIN_VALUE;
+    }
+
+    private static boolean isCharacter(char c) {
+        return LOWER_CASE_MIN_VALUE <= c && c <= LOWER_CASE_MAX_VALUE ||
+                UPPER_CASE_MIN_VALUE <= c && c <= UPPER_CASE_MAX_VALUE;
+    }
+
 
 }
