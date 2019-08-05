c06d47c2b250390c2481829faa496c77a565f7e2
huafresh <2587168099>
2019-08-05 12:42:32 +0800

    加一

--- leetcode_core/src/main/java/com/hua/leetcode_core/LeetCode_company.java
+++ leetcode_core/src/main/java/com/hua/leetcode_core/LeetCode_company.java
@@ -17,11 +17,11 @@ public class LeetCode_company {
         // 2、最后可能包含若干空格
         int len = s.length();
         int count = 0;
-        for(int i=len-1;i>=0;i--){
-            if(s.charAt(i)!=' '){
+        for (int i = len - 1; i >= 0; i--) {
+            if (s.charAt(i) != ' ') {
                 count++;
-            } else{
-                if(count!=0){
+            } else {
+                if (count != 0) {
                     break;
                 }
             }
@@ -29,4 +29,51 @@ public class LeetCode_company {
         return count;
     }
 
+    /**
+     * <a href = "https://leetcode-cn.com/problems/plus-one/submissions/">加一</a>
+     */
+    public int[] plusOne(int[] digits) {
+        // 此题的难点在于考虑进位的问题。
+        // return plusOneIndex(digits,digits.length-1);
+
+        // 以上是递归解法，略微有些复杂。。。
+        // 看了题解发现针对此题其实循环就可以了。
+
+        int len = digits.length;
+        for (int i = len - 1; i >= 0; i--) {
+            int plus = digits[i] + 1;
+            if (plus > 9) {
+                // 这里就是只用循环就可以的根本原因，因为题目要求加1，而进位也是加1，
+                // 所以这里只需把i所在位置置0，下一次循环进位就会得到体现。
+                digits[i] = 0;
+            } else {
+                digits[i] = plus;
+                // 不需要进位了，直接返回
+                return digits;
+            }
+        }
+        int[] newDigits = new int[len + 1];
+        //这里都不需要copy，因为进位到这里的话，只有高位是1，后面全是0。
+        newDigits[0] = 1;
+        return newDigits;
+    }
+
+    private int[] plusOneIndex(int[] digits, int index) {
+        if (index == -1) {
+            //已经进到最高位
+            int[] newDigits = new int[digits.length + 1];
+            newDigits[0] = 1;
+            System.arraycopy(digits, 0, newDigits, 1, digits.length);
+            return newDigits;
+        }
+        int plus = digits[index] + 1;
+        if (plus > 9) {
+            digits[index] = 0;
+            return plusOneIndex(digits, index - 1);
+        } else {
+            //不需要进位
+            digits[index] = plus;
+            return digits;
+        }
+    }
 }
