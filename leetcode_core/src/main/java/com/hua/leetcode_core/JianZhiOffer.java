069dda6dc6fb75cf52182bb9986189c5a5180c29
huafresh <2587168099>
2020-03-19 09:48:57 +0800

    旋转数组的最小数字

--- leetcode_core/src/main/java/com/hua/leetcode_core/JianZhiOffer.java
+++ leetcode_core/src/main/java/com/hua/leetcode_core/JianZhiOffer.java
@@ -1,6 +1,7 @@
 package com.hua.leetcode_core;
 
 import java.util.ArrayList;
+import java.util.Stack;
 
 /**
  * @author zhangsh
@@ -13,6 +14,11 @@ public class JianZhiOffer {
     public static void main(String[] args) {
         boolean exist = new JianZhiOffer().find(7, new int[][]{{1, 2, 8, 9}, {2, 4, 9, 12}, {4, 7, 10, 13}, {6, 8, 11, 15}});
         System.out.println("exist = " + exist);
+
+        int[] pre = new int[]{1, 2, 4, 7, 3, 5, 6, 8};
+        int[] in = new int[]{4, 7, 2, 1, 5, 3, 8, 6};
+        TreeNode root = rebuildBinaryTree(pre, in);
+
     }
 
     /**
@@ -98,4 +104,116 @@ public class JianZhiOffer {
             this.val = val;
         }
     }
+
+    public static TreeNode rebuildBinaryTree(int[] pre, int[] in) {
+        if (pre.length == 0 || in.length == 0) {
+            return null;
+        }
+        return rebuildBinaryTreeRecursive(pre, 0, pre.length - 1, in, 0, in.length - 1);
+    }
+
+    private static TreeNode rebuildBinaryTreeRecursive(int[] pre, int preStart, int preEnd,
+                                                       int[] in, int inStart, int inEnd) {
+        if (preStart > preEnd || inStart > inEnd) {
+            return null;
+        }
+        int rootVal = pre[preStart];
+        TreeNode root = new TreeNode(rootVal);
+        for (int i = inStart; i <= inEnd; i++) {
+            if (in[i] == rootVal) {
+                root.left = rebuildBinaryTreeRecursive(pre, preStart + 1, preStart + i - inStart,
+                        in, inStart, i - 1);
+                root.right = rebuildBinaryTreeRecursive(pre, preStart + i - inStart + 1, preEnd,
+                        in, i + 1, inEnd);
+            }
+        }
+        return root;
+    }
+
+    private static class TreeNode {
+        TreeNode left = null;
+        TreeNode right = null;
+        int val;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    /**
+     * 用两个栈来实现一个队列，完成队列的Push和Pop操作。 队列中的元素为int类型。
+     */
+    public class Solution {
+        Stack<Integer> stack1 = new Stack<Integer>();
+        Stack<Integer> stack2 = new Stack<Integer>();
+
+        public void push(int node) {
+            stack1.push(node);
+        }
+
+        public int pop() {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+            int pop = -1;
+            if (!stack2.isEmpty()) {
+                pop = stack2.pop();
+            }
+            while (!stack2.isEmpty()) {
+                stack1.push(stack2.pop());
+            }
+            return pop;
+        }
+
+        // 这是题解的方式，省略了拷贝回去的操作：
+        public int pop2() {
+            if (stack1.empty() && stack2.empty()) {
+                throw new RuntimeException("Queue is empty!");
+            }
+            if (stack2.empty()) {
+                while (!stack1.empty()) {
+                    stack2.push(stack1.pop());
+                }
+            }
+            return stack2.pop();
+        }
+    }
+
+    /**
+     * 把一个数组最开始的若干个元素搬到数组的末尾，我们称之为数组的旋转。
+     * 输入一个非递减排序的数组的一个旋转，输出旋转数组的最小元素。
+     * 例如数组{3,4,5,1,2}为{1,2,3,4,5}的一个旋转，该数组的最小值为1。
+     * NOTE：给出的所有元素都大于0，若数组大小为0，请返回0。
+     */
+    public int minNumberInRotateArray(int[] array) {
+        // 要仔细阅读此题的题意：首先是有一个非递减（即递增）的序列，然后旋转该数组作为此方法的输入
+        // 所以遍历虽然可以解决此问题，但是效率不高，因为旋转后的数组有部分子序列是有序的，
+        // 所以此题的关键是考察二分法提高查找效率。
+        if (array.length == 0) {
+            return 0;
+        }
+        int left = 0, right = array.length - 1;
+        while (left < right) {
+            // 旋转后的序列其实存在两个有序的子序列，[left, right]区间内的元素一定满足：
+            // array[left] > array[right]
+            if (array[left] < array[right]) {
+                return array[left];
+            }
+            int mid = left + (right - left) / 2;
+            if (array[mid] > array[left]) {
+                // 如果mid比left大，则left可以移到mid+1的位置缩小区间
+                // 这里加1的话很有可能导致赋值后的left就是最小值的位置，
+                // 不过没关系，每次while循环的开始我们已经判断了array[left] < array[right]
+                left = mid + 1;
+            } else if (array[mid] < array[right]) {
+                // 这里不能减1，减1的话可能会把最小值给跳过去了。
+                right = mid;
+            } else {
+                // 相等时，说明mid已经等于left了，因为二分最后的宿命就是mid=left。
+                ++left;
+            }
+        }
+        return array[left];
+    }
+
 }
