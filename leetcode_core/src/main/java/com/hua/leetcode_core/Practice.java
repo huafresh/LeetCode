7892e38961754f4e30507bddf9623aa9a06c505c
clydeazhang <clydeazhang@tencent.com>
2022-05-15 10:27:32 +0800

    买卖股票的最佳时机II

--- leetcode_core/src/main/java/com/hua/leetcode_core/Practice.java
+++ leetcode_core/src/main/java/com/hua/leetcode_core/Practice.java
@@ -14,9 +14,12 @@ public class Practice {
 
     public static void main(String[] args) {
         Practice practice = new Practice();
-        int[] nums = new int[]{4, 0, 0, 0, 0, 0};
-        int[] nums2 = new int[]{1, 2, 3, 5, 6};
-        practice.merge(nums, 1, nums2, 5);
+//        int[] nums = new int[]{4, 0, 0, 0, 0, 0};
+//        int[] nums2 = new int[]{1, 2, 3, 5, 6};
+//        practice.merge(nums, 1, nums2, 5);
+
+        int[] prices = new int[]{7, 1, 3, 5, 4, 8};
+        int profit = practice.maxProfit(prices);
     }
 
     /**
@@ -97,5 +100,44 @@ public class Practice {
             }
             return copy;
         }
+    }
+
+    /**
+     * 买卖股票的最佳时机II: https://leetcode.cn/leetbook/read/top-interview-questions-easy/x2zsx1/
+     */
+    public int maxProfit(int[] prices) {
+        // 很惭愧，这一题不管是贪心法还是动态规划，自己都没有解出来。以下是评论区的思路：
+        // 动态规划：
+        // 用dp[n][0]表示第n天交易完后手里没有股票的最大利润；
+        // 用dp[n][1]表示第n天交易完后手里有股票的最大利润。
+        // 如果当天交易完手里没有股票，则分两种情况：
+        // 第一种是前一天手里有股票，当天卖了，那么最大利润是：dp[n-1][1] + prices[n]
+        // 第二种是前一天手里没有股票，当前无操作，那么最大利润等于dp[n-1][0]
+        // 由此得到当天交易完手里没股票的状态转移方程：
+        // dp[n][0] = Math.max(dp[n-1][1] + prices[n], dp[n-1][0])
+        // 我们可以同理去分析当天交易完手里还有股票的情况：
+        // dp[n][1] = Math.max(dp[n-1][0] - prices[n], dp[n-1][1])
+        // PS: 对于上面dp[n][1]的状态转移公式我是持有疑问的：前一天持有股票，当天也没卖，那么最大利润不应该是dp[n-1][1]+prices[n]吗
+
+//        if (prices.length == 0) return 0;
+//        int hold = -prices[0];
+//        int noHold = 0;
+//        for (int i = 1; i < prices.length; i++) {
+//            int newNoHold = Math.max(noHold, hold + prices[i]);
+//            hold = Math.max(hold, noHold - prices[i]);
+//            noHold = newNoHold;
+//        }
+//        return noHold;
+
+        // 贪心算法：
+        // 如果把每天的价格在坐标上画成折线，其实就是妥妥的k线图嘛，那么最大收益其实就是收获所有上涨，躲过所有下跌。
+        // 因此我们可以遍历数组，通过对比今天和明天的价格确定明天会不会涨，会涨则今天买入，明天卖出（或者一直涨就一直持有）。
+        int profit = 0;
+        for (int i = 0; i < prices.length - 1; i++) {
+            if (prices[i + 1] > prices[i]) {
+                profit += prices[i + 1] - prices[i];
+            }
+        }
+        return profit;
     }
 }
